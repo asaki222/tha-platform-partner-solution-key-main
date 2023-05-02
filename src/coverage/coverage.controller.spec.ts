b68b7451ec99db0e08e174bdbf7d5b85b918066d
Aditi <asaki444@gmail.com>
2023-05-02 17:41:56 -0400

    fix: to pass tests to confirm service and controller are defined

--- src/coverage/coverage.controller.spec.ts
+++ src/coverage/coverage.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoverageController } from './coverage.controller';
 import { CoverageService } from './coverage';
+import { HttpModule } from '@nestjs/axios';
 
 describe('CoverageController', () => {
   let controller: CoverageController;
@@ -9,6 +10,7 @@ describe('CoverageController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CoverageController],
       providers: [CoverageService],
+      imports: [HttpModule]
     }).compile();
 
     controller = module.get<CoverageController>(CoverageController);
